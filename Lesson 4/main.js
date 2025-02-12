8b9db8f110767d74371f52212241149a5fa0e6ac
YuriiShepel <yurii.shepel.zp@gmail.com>
2025-02-12 12:32:45 +0100

    JS - Lesson 4

---
+++ Lesson 4/main.js
@@ -0,0 +1,159 @@
+//#I2XsG6f
+
+function rectSquare(a, b){
+    return a * b;
+}
+console.log(rectSquare(4,5));
+
+//#ETGAxbEn8l
+console.log('');
+
+function circleSquare(r){
+    return 3.14 * r * r;
+}
+console.log(circleSquare(5));
+
+//#Mbiz5K4yFe7
+console.log('');
+
+function tubeSquare(r, h){
+    return 3.14 * 2 * r * h;
+}
+console.log(tubeSquare(5, 20));
+
+//#SIdMd0hQ
+
+function arrayIterator(arr){
+    for (const arrElement of arr) {
+        console.log(arrElement);
+    }
+}
+arrayIterator([1,2,3,4,5,6]);
+
+//#59g0IsA
+
+function createP(content){
+    document.write(`<p>${content}</p>`);
+}
+createP('Simple text');
+
+//#hOL6126
+
+function createList(content){
+    document.write(`
+        <ul>
+            <li>${content}</li>
+            <li>${content}</li>
+            <li>${content}</li>
+        </ul>`);
+}
+createList('List content');
+
+//#0Kxco1edSN
+
+function createLongList(content, length){
+    let list = '<ul>';
+    for (let i = 0; i < length; i++) {
+        list += `<li>${content}</li>`;
+    }
+    list += '</ul>';
+    document.write(list);
+}
+
+createLongList('Long list content', 10);
+
+//#gEFoxMMO
+
+function createMixedList(content){
+    let list = '<ul>';
+    for (const contentElement of content) {
+        list += `<li>${contentElement}</li>`;
+    }
+    list += '</ul>';
+    document.write(list);
+}
+
+createMixedList(['text1', 'text2', true, 34, 'text3', 12, false, 90]);
+
+//#bovDJDTIjt
+
+function createObjectList(objects){
+    let result = '';
+    for (const object of objects) {
+        result += `<div>Id - ${object.id}, name - ${object.name}, age - ${object.age}</div>`;
+    }
+    document.write(result);
+}
+
+let objects = [
+    {id: 1, name: 'First Name', age: 20},
+    {id: 2, name: 'Second Name', age: 21},
+    {id: 3, name: 'Third Name', age: 22},
+    {id: 4, name: 'Fourth Name', age: 23},
+    {id: 5, name: 'Fifth Name', age: 24}
+];
+createObjectList(objects);
+
+//#pghbnSB
+console.log('');
+
+function getMin(numbers){
+    let min = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] < min)
+            min = numbers[i];
+    }
+    return min;
+}
+
+let numbers = [23, 32, 3, 5, 12, 45, 76, 2, 10];
+console.log(getMin(numbers));
+
+//#EKRNVPM
+console.log('');
+
+function sum(arr){
+    let sum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+numbers = [23, 32, 3, 5, 12, 45, 76, 2, 10];
+console.log(sum(numbers));
+
+//#kpsbSQCt2Lf
+console.log('');
+
+function swap(arr, index1, index2){
+    let temp1 = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp1;
+    return arr;
+}
+
+let arr = [11,22,33,44];
+console.log(arr);
+arr = swap(arr, 0, 1);
+console.log(arr);
+
+//#mkGDenYnNjn
+console.log('');
+
+function exchange(amount,currencies,targetCurrency){
+    for (const currency of currencies) {
+        if (currency.currency === targetCurrency){
+            return amount / currency.value;
+        }
+    }
+    return -1;
+}
+
+let result = exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD');
+console.log(result >= 0 ? result : 'Target Currency not found');
+
+
+
+
+
